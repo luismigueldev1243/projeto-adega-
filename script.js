9c9d26d8f96f846587eeae22696e1fd2a242cb0e
luis miguel <luismiguelbrancalion2@gmail.com>
2025-01-23 12:58:43 -0300

    adicionei uma cacetada de coisa novas e coloquei RESPONSIVIDADE

--- script.js
+++ script.js
@@ -1,47 +1,91 @@
 const products = {
-    "Cerveja Antártica Boa Lata": 4.50,
-    "Cerveja Amstel Lata 350 ML": 4.50,
-    "Cerveja império Lata 350ML": 4.50,
-    "Cerveja Budweiser Lata 350ML": 6.00,
-    "Cerveja Antártica GF 300ML": 4.00,
-    "Cerveja Heineken LT": 6.50,
-    "Cerveja Heineken LN": 8.00,
-    "Cerveja Skol GF 300 ML": 4.00,
-    "Heineken zero": 6.50,
-    "Cerveja Corona": 9.00,
-    "Coca lata": 5.00,
-    "Guaraná lata": 5.00,
-    "Água": 2.50,
-    "Água com gás": 3.50,
-    "Smirnoff ice": 10.00,
-    "51 ice": 7.50,
-    "Energético BALLY 2 litros": 15.00,
-    "Energético Furioso 2 litros": 15.00,
-    "Red Bull LT 255ML": 12.00,
-    "Whsiky Red label 1 litro": 110.00,
-    "Whisky Red label 700 ML": 90.00,
-    "Whisky Jack Daniels": 159.90,
-    "Whisky cavalo branco": 100.00,
-    "Whisky Ballantines": 110.00,
-    "Whisky Passport honey": 60.00,
-    "Whisky Gold Label": 299.00,
-    "Whisky Chivas": 169.00,
-    "Gin intencion": 30.00,
-    "Combo Red label": 140.00,
-    "Combo cavalo branco": 130.00,
-    "Combo Gin intencion": 65.00,
-    "Ficha":2.0,
+    "Ficha": { price: 2.00, img: 'none' },
+    "Palheiro solto": {price:2.00,img:'palheiro.png'},
+    "Palheiro Caixa": {price:28.00,img:'palheiro.png'},
+    "Antártica Boa Lata": { price: 4.50, img: 'antart.boal.png' },
+    "Amstel Lata 350 ML": { price: 4.50, img: 'amstel.png' },
+    "império Lata 350ML": { price: 4.50, img: 'imperiolata.png' },
+    "Budweiser Lata 350ML": { price: 6.00, img: 'budwieser.png' },
+    "Antártica GF 300ML": { price: 4.00, img: 'antar.300ml.png' },
+    "Heineken LT": { price: 6.50, img: 'heinekenLT.png' },
+    "Heineken LN": { price: 8.00, img: 'heinekenLN.png' },
+    "Skol GF 300 ML": { price: 4.00, img: 'skolGF300ml.png' },
+    "Heineken zero": { price: 6.50, img: 'heineken0.png' },
+    "Corona": { price: 9.00, img: 'corona.png' },
+    "Corote": {price:3.50,img:'corote.png'},
+    "Coca lata": { price: 5.00, img: 'coca-lata.png' },
+    "Guaraná lata": { price: 5.00, img: 'guarana-lata.png' },
+    "Água": { price: 2.50, img: 'agua.png' },
+    "Água com gás": { price: 3.50, img: 'aguacgas.png' },
+    "Smirnoff ice": { price: 10.00, img: 'smirnofICE.png' },
+    "51 ice": { price: 7.50, img: '51ice.png' },
+    "Energético BALLY 2 L.": { price: 15.00, img: 'e.bally.png' },
+    "Energético Furioso 2 L": { price: 15.00, img: 'energ.furious.png' },
+    "Red Bull LT 255ML": { price: 12.00, img: 'redBULLlt.png' },
+    "Whisk. Red label 1 L.": { price: 110.00, img: 'w.redl.1l.png' },
+    "Whisk. Red label 7 mL.": { price: 90.00, img: 'w.redl.700ml.png' },
+    "Whisk. Jack Daniels": { price: 159.90, img: 'w.jackd.png' },
+    "wh.cavalo branco": { price: 100.00, img: 'w.cav.branco.png' },
+    "Whisk. Ballantines": { price: 110.00, img: 'w.balantines.png' },
+    "Whis. Passport h.": { price: 60.00, img: 'w.pasporth.png' },
+    "Whisk. Gold Label": { price: 299.00, img: 'w.goldl.png' },
+    "Whisk. Chivas": { price: 169.00, img: 'w.chivas.png' },
+    "Gin intencion": { price: 30.00, img: 'ginintenc.png' },
+    "Combo Red label": { price: 140.00, img: 'none' },
+    "Combo cav. branco": { price: 130.00, img: 'none' },
+    "Combo Gin intencion": { price: 65.00, img: 'none' },
 };
 
 
+const prodDIV = document.getElementById('prod-div');
 const mainDiv = document.getElementById('main-div');
 const produtosInput = document.getElementById('produto');
 const datalistProdutos = document.getElementById('produtos');
 const searchButton = document.getElementById('serch-pd');
-const productDiv = document.getElementById('product-div');
+const productDiv = document.getElementById('product-search-div');
 const productText = document.getElementById('product');
 const priceText = document.getElementById('price');
 
+for(const item in products){
+    const prodITMdiv = document.createElement('div')
+  const itm = document.createElement('p')
+  const itmprice = document.createElement('p')
+  const itmIMG= document.createElement('img')
+  
+  prodITMdiv.addEventListener('click',()=>{
+    produtosInput.value = itm.textContent
+    searchButton.click()
+  })
+  
+
+  prodITMdiv.id ='product-div'
+  itm.id ='prod-name'
+  itmprice.id ='prod-price'
+  
+  let preco = JSON.stringify(products[item].price)
+  let hpoints = preco.includes(".")
+  if(hpoints){
+    itm.innerHTML = item
+    itmprice.innerHTML = 'R$ ' + preco.replace('.',',')
+  }else{
+    itm.innerHTML = item.replace('Cerveja','')
+    itmprice.innerHTML = 'R$ ' + preco + ',00'
+  }
+  if(products[item].img == 'none'){
+    itmIMG.style.content = 'url(not-finded.png)'
+   
+    itmIMG.style.width = 75 +'%'
+  }else{
+    itmIMG.style.content = `url(${products[item].img})`
+  }
+  prodITMdiv.append(itmIMG)
+  prodITMdiv.append(itm)
+  prodITMdiv.append(itmprice)
+  prodDIV.append(prodITMdiv)
+  if(itmprice.value == ''){
+    itmprice.parentNode.remove()
+  }
+}
 
 searchButton.addEventListener('click',()=>{
     let iptValue = produtosInput.value
@@ -49,7 +93,7 @@ searchButton.addEventListener('click',()=>{
         alert('por favor digite algo no input')
     }else if (iptValue != ''){
          productText.innerHTML = iptValue
-         priceText.innerHTML ='<i class="fa-solid fa-brazilian-real-sign"></i>: ' +products[iptValue]
+         priceText.innerHTML ='<i class="fa-solid fa-brazilian-real-sign"></i>: ' +products[iptValue].price
         }
 })
 
@@ -64,7 +108,7 @@ totalSell.innerHTML = 'Venda total: $' +JSON.parse(localStorage.getItem('sells')
 
 isSelled.addEventListener('click',()=>{
     let totalfromls =JSON.parse(localStorage.getItem('sells'))
-    totalfromls.total = totalfromls.total + products[produtosInput.value]
+    totalfromls.total = totalfromls.total + products[produtosInput.value].price
     localStorage.setItem('sells',JSON.stringify(totalfromls))
     totalSell.innerHTML='venda total: $' + totalfromls.total
 })
